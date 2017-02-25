b642c0ec9d823b195f3f4c24102b5a06005f8eab
Jordan Knight <jakkaj@outlook.com>
2017-02-26 08:27:46 +1100

    Temp video files.
    Encode time in to the video

--- Console/StreamRipper/Program.cs
+++ Console/StreamRipper/Program.cs
@@ -33,21 +33,23 @@ namespace StreamRipper
                 var pi = new ProcessStartInfo();
 
                 pi.FileName = @"E:\Tools\ffmpeg\ffmpeg-20170112-6596b34-win64-static\bin\ffmpeg.exe";
-                var fn = Path.Combine(Directory.GetCurrentDirectory(), DateTime.UtcNow.Ticks + ".mp4");
+
+                var fn = Path.GetTempFileName();
 
                 var arguments =
-                    $"-y -i {address} -t 600 -r 3 -c:v libx264 -b 50000 -pix_fmt yuv420p -f mp4 \"{fn}\"";
+                    $"-y -i {address}  -t 15  -r 3 -vf drawtext=\"fontfile=C\\\\:/Windows/Fonts/arial.ttf:text='%{{localtime\\:%X}}:fontcolor=yellow'\" -c:v libx264 -b 50000 -pix_fmt yuv420p -f mp4 \"{fn}\"";
 
                 pi.Arguments = arguments;
                 pi.WorkingDirectory = Directory.GetCurrentDirectory();
                 pi.UseShellExecute = false;
                 var process = Process.Start(pi);
 
-                process.WaitForExit((int)TimeSpan.FromSeconds(620).TotalMilliseconds);
+                process.WaitForExit((int)TimeSpan.FromSeconds(320).TotalMilliseconds);
 
                 if (!process.HasExited)
                 {
                     process.Kill();
+                    Thread.Sleep(2000);
                 }
 
                 if (!File.Exists(fn))
@@ -55,6 +57,8 @@ namespace StreamRipper
                     Thread.Sleep(5000);
                     continue;
                 }
+
+                Thread.Sleep(1000);
 
                 var data = File.ReadAllBytes(fn);
 
